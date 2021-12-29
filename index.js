66fceee2ed4f514adb28b0184692a049a4fcea64
chaimvaid <chaim@daneltech.com>
2021-12-30 01:12:39 +0200

    add use field to index

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export {Form} from './form/form';
 export {Field} from './form/field';
 export {Fields} from './form/fields';
 export {useForm} from './form/useForm';
+export {useField} from './form/useField';
